e5e7c440ba1254ff8b34a64efbc1e3050d0e7344
Jose Vasquez <jose.vasquez.a@utec.edu.pe>
2022-01-30 22:15:35 -0500

    Added numpy tutorial
    first tutorial of data analysis video

---
+++ numpy_tutorial.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+#TRABAJA IGUAL QUE PYTHON LIST
+
+if __name__== "__main__":
+    a = np.array([[1,2,3,4], [2,4,3,6]])
+    b = np.arange(5)
+    b += 100
+    print(a[-1][0:2])
+    print(a[0,2])
+    print(b)
+    print(b[b>=101])                        #Se puede hacer comparaciones dentro de los arrays para filtrar mas rapido los datos
