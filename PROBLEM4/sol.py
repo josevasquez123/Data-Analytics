2da7fa9de22850bec4d7d5edbbd07448a787d3eb
Jose Vasquez <jose.vasquez.a@utec.edu.pe>
2022-03-02 15:20:47 -0500

    finish excel exercise

--- PROBLEM4/sol.py
+++ PROBLEM4/sol.py
@@ -1,12 +1,20 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def addlabels(x,y):
+    for i in range(len(x)):
+        plt.text(i-0.1,y[i]+5,y[i], fontweight='bold')
+
 if __name__=="__main__":
     sheets = pd.read_excel("products.xlsx", sheet_name=[0,2])
 
     sheets[0]['brand'] = sheets[0]['brand'].str.lower()
 
     sheets[0]['merchant_id'] = sheets[0]['merchant_id'].astype(str)
+
+    id_description = sheets[2]
+
+    id_description['merchant_id'] = id_description['merchant_id'].astype(str)
 
     #What is the brand with highest sales
 
@@ -40,17 +48,28 @@ if __name__=="__main__":
 
     #Show 5 merchant with highest sales
 
-    merchants = sheets[0].groupby('merchant_id').count()
+    merchants = sheets[0].groupby('merchant_id').count()                #Cantidad de productos vendidos agrupados por el vendedor
 
-    x = merchants['name'].nlargest(5)
+    x = merchants['name'].nlargest(5)                                   #Los 5 mayores vendedores
 
-    lower_merchants_id = x.index.values
+    lower_merchants_id = x.index.values                                 #El ID de los vendedores
 
-    sales_quantity = x.values
+    sales_quantity = x.values                                           #Su numero de ventas
+    
+    #Obtener el nombre de los vendedores mediante el ID
+
+    names = []
+
+    for descrip in lower_merchants_id:
+        x = id_description[id_description['merchant_id']==descrip]
+
+        names.append(x.iloc[0]['merchant'])           
 
     plt.style.use('seaborn-whitegrid')
 
-    plt.bar(lower_merchants_id, sales_quantity)
+    plt.bar(names, sales_quantity)
+
+    addlabels(names, sales_quantity)
 
     plt.title("5 highest merchants sales")
 
@@ -59,4 +78,6 @@ if __name__=="__main__":
     plt.xlabel("Merchant ID")
 
     plt.show()    
+
+
 
